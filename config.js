a22dc2c466f6c439db65670887a3a8193140b1f3
Cogons <cogons@qq.com>
2018-02-08 16:11:52 +0800

    change to 6 rare degrees

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     reverse:true, // 从后往前购买
     yzm_method:"manual", // "baidu" || "manual"
-    threshold:[490,490,490,490,490], // 从普通到神话的购买上限
+    threshold:[1400,1400,1400,1400,1400,1400], // 普通、稀有、卓越、史诗、神话、传说
     query_amount:3000, //查询次数
     show_affordable_message:true //显示是否买得起的信息
 }
